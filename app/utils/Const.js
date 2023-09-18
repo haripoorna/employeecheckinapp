6dd109ec733d6329a30bfef192d7996b503bfd76
hari.kalahasti <Hari.Kalahasti@powerschool.com>
2023-09-18 15:21:59 -0400

    update

--- app/utils/Const.js
+++ app/utils/Const.js
@@ -5,7 +5,7 @@
 //   longitudeDelta: 0.0421,
 // };
 export  const officeLocation = {
-  latitude: 18.10343934555502,
+  latitude: 18.10343934555502ß,
   longitude: 83.4005718460291,
   latitudeDelta: 0.0922,
   longitudeDelta: 0.0421,
